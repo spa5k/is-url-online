31e62414104fa2f83de1ab359218439b9cf6b869
Unbuttun <unbuttun@gmail.com>
2021-08-22 00:29:32 +0530

    feat(docs): ✨ Improving JS Doc support

--- src/modules/isUrlString.ts
+++ src/modules/isUrlString.ts
@@ -1,5 +1,9 @@
 import URL from "url";
-
+/**
+ * @param  {string} url
+ * @returns boolean
+ * @description Checks whether the string mentioned is of a URL or not (offline).
+ */
 export const isUrlString = (url: string): boolean => {
   if (typeof url !== "string") {
     throw new TypeError(`Expected a string, got ${typeof url}`);
