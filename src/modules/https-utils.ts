6f6b3e714f506fd169db3bfdacf23d74b84ae762
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-08-08 17:02:26 -0700

    feat: use the built-in https package for testing the URLs

---
+++ src/modules/https-utils.ts
@@ -0,0 +1,33 @@
+import { type IncomingMessage } from "http";
+import { request } from "https";
+
+function isStatusOK(result: IncomingMessage) {
+  return (
+    result.statusCode !== undefined &&
+    result.statusCode >= 200 &&
+    result.statusCode <= 399
+  );
+}
+
+/**
+ * Check if a request for the given URL works
+ *
+ * @param url The given URL
+ * @returns A promise that resolves to a boolean indicating if the URL works
+ *
+ * @note the function doesn't throw an exception
+ */
+export function requestWorks(
+  url: string,
+  method: string | undefined
+): Promise<boolean> {
+  return new Promise<boolean>((resolve) => {
+    try {
+      request(url, { method }, (result) => {
+        return resolve(isStatusOK(result));
+      });
+    } catch {
+      return resolve(false);
+    }
+  });
+}
